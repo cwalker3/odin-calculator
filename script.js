2d8ed01e444970cb33a05920d3c8c508edcc87ef
Cory Walker <tepoen98@gmail.com>
2022-08-16 14:07:24 -0700

    add function that puts inputs into an array and updates display

--- script.js
+++ script.js
@@ -20,3 +20,42 @@ const operate = (a, operator, b) => {
             return divide(a, b);
     }
 };
+//array to store inputs
+let inputs = [];
+//variable to store display text
+let displayText = document.querySelector('.display');
+
+//variables to use in the operate function
+let a;
+let b;
+let operator;
+
+//function that takes input array and gets values to use for operate function
+let  pairInputs = (inputs) => {
+    
+}
+
+//function that puts inputs into input array and updates display
+let updateDisplay = (e) => {
+    let buttonValue = e.target.textContent;
+    if (buttonValue === '=')
+        pairInputs(inputs);
+    else if (buttonValue === 'clear')
+        inputs = [];
+    else if (Number(buttonValue) || buttonValue === '0') 
+        inputs.push(buttonValue);
+    else {
+        inputs.push(' ');
+        inputs.push(buttonValue);
+        inputs.push(' ')
+    }
+    displayText.textContent = inputs.join('');
+};
+
+let buttons = document.querySelectorAll('button');
+for (let button of buttons) {
+    button.addEventListener('click', updateDisplay);
+}
+
+
+
